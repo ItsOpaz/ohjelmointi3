6bd640b6c85d21d0dbce76f74ec189c6c3ac2254
ItsOpaz <olli.pettersson@outlook.com>
2020-11-03 20:17:20 +0200

    added new class inherited from iCity interface, added logic and city creation to main (not working properly because of city class missing functions)

---
+++ Game/city.h
@@ -0,0 +1,31 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include "interfaces/icity.hh"
+
+#include <QTime>
+
+namespace Students {
+
+class City : public Interface::ICity
+{
+public:
+    City();
+
+    // ICity interface
+public:
+    void setBackground(QImage &basicbackground, QImage &bigbackground);
+    void setClock(QTime clock);
+    void addStop(std::shared_ptr<Interface::IStop> stop);
+    void startGame();
+    void addActor(std::shared_ptr<Interface::IActor> newactor);
+    void removeActor(std::shared_ptr<Interface::IActor> actor);
+    void actorRemoved(std::shared_ptr<Interface::IActor> actor);
+    bool findActor(std::shared_ptr<Interface::IActor> actor) const;
+    void actorMoved(std::shared_ptr<Interface::IActor> actor);
+    std::vector<std::shared_ptr<Interface::IActor> > getNearbyActors(Interface::Location loc) const;
+    bool isGameOver() const;
+};
+}
+
+#endif // CITY_H
--- Game/main.cc
+++ Game/main.cc
@@ -1,9 +1,16 @@
 #include <QApplication>
+#include "core/logic.hh"
+#include "city.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Q_INIT_RESOURCE(offlinedata);
-
+    CourseSide::Logic game;
+    game.fileConfig();
+    std::shared_ptr<Students::City> Tampere(new Students::City());
+    Tampere->setClock(QTime::currentTime());
+    game.takeCity(Tampere);
+    game.finalizeGameStart();
     return a.exec();
 }
